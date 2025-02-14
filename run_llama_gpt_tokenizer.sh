8d74523115bc599dec9d5276d9058362f892863c
prasannamayil <prasanna.mayilvahanan@gmail.com>
2025-02-14 16:45:41 +0100

    more configs

---
+++ run_llama_gpt_tokenizer.sh
@@ -0,0 +1,8 @@
+# Get arguments
+NPROC=$1
+CONFIG=$2
+API_KEY=$3
+source ~/.lingua_241216/bin/activate
+export NCCL_IB_DISABLE=1  # Disable InfiniBand if not needed
+export NCCL_P2P_DISABLE=1  # Disable P2P if causing issues
+torchrun --nproc-per-node $NPROC -m apps.main.train config=apps/main/configs/$CONFIG
